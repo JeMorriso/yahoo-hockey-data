b792e2f56da962d61a1955ad8c0687f78c53b966
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-03-14 19:14:05 -0700

    hide consumer key and secret using utility script

---
+++ scripts/utils/dotenv_to_json.py
@@ -0,0 +1,10 @@
+import json
+import os
+
+credentials = {
+    'consumer_key': os.getenv("CONSUMER_KEY"),
+    'consumer_secret': os.getenv("CONSUMER_SECRET")
+}
+
+with open('credentials.json', 'w') as creds_file:
+    json.dump(credentials, creds_file, indent=2)
