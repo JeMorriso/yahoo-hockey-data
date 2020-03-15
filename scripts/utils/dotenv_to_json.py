0c4ddf0c2b4c1e7978bfb289a1de1085d8a3a48b
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-03-14 19:18:39 -0700

    heroku debugging

--- scripts/utils/dotenv_to_json.py
+++ scripts/utils/dotenv_to_json.py
@@ -6,5 +6,5 @@ credentials = {
     'consumer_secret': os.getenv("CONSUMER_SECRET")
 }
 
-with open('credentials.json', 'w') as creds_file:
+with open('../../credentials.json', 'w') as creds_file:
     json.dump(credentials, creds_file, indent=2)
