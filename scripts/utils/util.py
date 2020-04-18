1851edc745b250b54711feb4f0bbec8628bc0aed
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-04-17 23:24:51 -0700

    Add height_to_int and int_to_height functions

--- scripts/utils/util.py
+++ scripts/utils/util.py
@@ -1,8 +1,12 @@
 import re
 
 def height_to_int(height_str):
-    re.findall(r'\d+', height_str)
-    pass
+    h = [int(s) for s in re.findall(r'\d+', height_str)]
+
+    return 12*h[0] + h[1]
 
 def int_to_height(height_inches):
-    pass
+    feet = height_inches/12
+    inches = height_inches%12
+
+    return f"{feet}\'{inches}\""
