bc901d7d67d2001b9038d7d4b7c73d28b31b7116
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-04-17 23:15:24 -0700

    Add utility script util.py

--- scripts/utils/test_yahoo_api.py
+++ scripts/utils/test_yahoo_api.py
@@ -1,6 +1,17 @@
 from yahooleaguedata import YahooLeagueData
+import json
+import os
+
 
 api_url = "https://fantasysports.yahooapis.com/fantasy/v2"
 league_url = api_url + "/leagues;league_keys=nhl.l.18538"
+
+old_path = os.getcwd()
+os.chdir('../../')
 league = YahooLeagueData(league_url)
+os.chdir(old_path)
+
+league.parse_raw_teams()
+league.parse_raw_rosters()
+
 
---
+++ scripts/utils/util.py
@@ -0,0 +1,8 @@
+import re
+
+def height_to_int(height_str):
+    re.findall(r'\d+', height_str)
+    pass
+
+def int_to_height(height_inches):
+    pass
