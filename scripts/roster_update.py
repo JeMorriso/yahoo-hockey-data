455566f0a0b24a0da657ae7b24644f2520f5aa47
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-04-21 16:39:06 -0700

    Add code for looping over weeks

--- scripts/roster_update.py
+++ scripts/roster_update.py
@@ -28,9 +28,20 @@ if __name__ == "__main__":
         if not args.end_date:
             args.end_date = datetime.datetime.today()
 
-        #TODO
-        # get weeks from db
-        sql = "select start_date from week"
+        # get start date's week from db
+        # TODO: if start in second half of week, it will still run over first half because we're getting start date from db
+        week, start_date, end_date = league_db_composite.db.get_week(args.start_date, league_db_composite.db.get_league_id(league_db_composite.league.parse_raw_league_data()))
+        # the database actually returns a date object since the type in the database is specified as 'date'
+        current_date = start_date
+        while current_date <= args.end_date:
+            league_db_composite.roster_player_update(current_date)
+            # the only week that doesn't start on a Monday is week 1
+            # if before Monday in week 1
+            if week == 1 and current_date <= start_date + datetime.timedelta(days=(6 - start_date.weekday())):
+                # this line sets current date to this Monday (relative to prev current date) (unless current day is Monday)
+                current_date += datetime.timedelta(days=(0 - current_date.weekday()) % 7)
+            else:
+                current_date += datetime.timedelta(days=7)
 
     # only updating rosters for 1 day
     else:
