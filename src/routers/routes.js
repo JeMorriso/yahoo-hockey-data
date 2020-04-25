7b267ac3ef22872422c584cf907b3efc4eebecf4
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-04-25 13:02:13 -0700

    Add .ejs header and footer partials

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -3,7 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Bobby Orr Image goes here');
 });
+
+router.get('/league', (req, res) => {
+  res.send('graphs and standings');
+})
+
+router.get('/matchup/:matchupId', (req, res) => {
+  res.send('graphs and standings');
+})
 
 module.exports = router;
