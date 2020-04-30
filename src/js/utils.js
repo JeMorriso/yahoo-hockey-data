d46f54c35b8332bb3bb34f9ee4c65485de7ffff7
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-04-30 14:18:29 -0700

    Add utility file and getDaysArray function

---
+++ src/js/utils.js
@@ -0,0 +1,8 @@
+const getDaysArray = function(start, end) {
+  for(var arr=[],dt=new Date(start); dt<=end; dt.setDate(dt.getDate()+1)){
+      arr.push(new Date(dt));
+  }
+  return arr;
+};
+
+module.exports = { getDaysArray }
