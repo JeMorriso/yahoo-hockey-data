d431de2ef485cce6d842b7b7549abc261e39b4ba
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-04-30 16:18:23 -0700

    Add flatpicker.js and webpack

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.set('view engine', 'ejs');
 
 // serve the public directory
 app.use(express.static(path.join(__dirname, '../public')));
+app.use("/dist", express.static(path.join(__dirname, '../dist')))
 
 // endpoints in routes file start with '/'
 app.use('/', routes)
@@ -21,7 +22,7 @@ app.locals.start_date = '2019-10-02';
 // default date is Today
   // ISO 8601 format same as start_date + time data so get substring
 // app.locals.end_date = new Date().toISOString().substring(0,10);
-app.locals.end_date = '2019-10-03'
+app.locals.end_date = '2020-03-02'
 
 // default to goals
 app.locals.category_snake_case = 'goals';
