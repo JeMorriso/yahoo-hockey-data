850e4c60cc9c0dd17c32dfc367dfb223b506ee15
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-05-06 18:02:16 -0700

    Chart styling

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,23 +1,5 @@
 var myChart;
 
-// use data provided from AJAX request to build chart
-// function expressions are NOT hoisted, so this must appear above any calling code
-const buildChartData = rawData => {
-    data = { labels: rawData.dates, datasets: [] }
-
-    // build structure for each dataset entry (each line in graph)
-    Object.entries(rawData.teamStats).forEach(([key, val], i) => {
-        data.datasets.push({
-            label: key,
-            data: val.cumulative,
-            borderColor: rawData.colours[i],
-            fill: false,
-            lineTension: 0
-        })
-    });
-    return data;
-}
-
 // reqData is start_date, end_date, and category, or empty (first page load)
 const getChartData = async reqData => {
     const response = await fetch('http://localhost:3000/chart', {
@@ -33,8 +15,23 @@ const getChartData = async reqData => {
     return resData;
 }
 
+// use data provided from AJAX request to build chart
+// function expressions are NOT hoisted, so this must appear above any calling code
+const buildChartData = rawData => {
+    data = { labels: rawData.dates, datasets: [] }
+
+    // build structure for each dataset entry (each line in graph)
+    Object.entries(rawData.teamStats).forEach(([key, val], i) => {
+        data.datasets.push({
+            label: key,
+            data: val.cumulative,
+            borderColor: rawData.colours[i],
+        })
+    });
+    return data;
+}
+
 function getChartDataWrapper(reqData) {
-    // cannot use await here because not inside an async function
     getChartData(reqData).then(data => {
         const chartData = buildChartData(data);
 
@@ -47,11 +44,20 @@ function getChartDataWrapper(reqData) {
             type: 'line',
             data: chartData,
             options: {
+                legend: {
+                    position: 'left',
+                    onHover: function(e) {
+                        e.target.style.cursor = 'pointer';
+                     },
+                     onLeave: (e) => {
+                         e.target.style.cursor = 'default';
+                     }
+                },
                 scales: {
                     yAxes: [{
                         ticks: {
                             // y-axis starts at 0
-                            beginAtZero: true
+                            // beginAtZero: true
                         }
                     }]
                 },
@@ -59,7 +65,8 @@ function getChartDataWrapper(reqData) {
                 responsive: true,
                 title: {
                     display: true,
-                    text: data.category
+                    text: data.category,
+                    fontSize: 24
                 }
             }
         });
@@ -68,3 +75,6 @@ function getChartDataWrapper(reqData) {
 
 getChartDataWrapper();
 Chart.defaults.global.defaultFontSize = 16;
+Chart.defaults.global.elements.line.tension = 0;
+Chart.defaults.global.elements.line.fill = false;
+Chart.defaults.global.elements.point.hitRadius = 10;
