6dddaec193ebb4f7895074fe026494fc8830e8ac
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-05-08 16:50:48 -0700

    Add cleanGroupbyTeamResult function

--- src/js/utils.js
+++ src/js/utils.js
@@ -4,10 +4,23 @@ const getDaysArray = function(start, end) {
   end = new Date(end.setHours(end.getHours() + 1));
 
   for (var arr=[],dt=new Date(start); dt<=end; dt.setDate(dt.getDate()+1)) {
-      console.log(dt);
       arr.push(dt.toISOString().substring(0,10));
   }
   return arr;
 };
 
-module.exports = { getDaysArray }
+const cleanGroupbyTeamResult = (teams, result, daysArray) => {
+  let teamDateStat = {};
+  teams.forEach(team => teamDateStat[team] = {});
+  for (team in teamDateStat) {
+    daysArray.forEach(day => {
+      teamDateStat[team][day] = 0;
+    })
+  }
+  for (row of result) {
+    teamDateStat[row.name][row.date] = row.stat;
+  }
+  return teamDateStat;
+} 
+
+module.exports = { getDaysArray, cleanGroupbyTeamResult }
