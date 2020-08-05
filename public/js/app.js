f13d4560840b44408a5d6a39a2d82d9fbd2727a0
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-08-05 13:45:12 -0700

    Rolling average radio button state persists across calls fetching chart data

--- public/js/app.js
+++ public/js/app.js
@@ -1,85 +1,87 @@
 var fp;
 
-document.getElementById("form-submit").addEventListener("click", _ => {
+// keeps track of the state of the chart radio buttons
+let isCumulative = true;
+
+document.getElementById('form-submit').addEventListener('click', (_) => {
   var start_date, end_date;
 
   console.log(fp.selectedDates);
   if (Array.isArray(fp.selectedDates) && fp.selectedDates.length) {
-    start_date = fp.selectedDates[0].toISOString().substring(0,10);
-    end_date = fp.selectedDates[1].toISOString().substring(0,10);
+    start_date = fp.selectedDates[0].toISOString().substring(0, 10);
+    end_date = fp.selectedDates[1].toISOString().substring(0, 10);
   }
-  
-  const result = document.getElementById("categories");
+
+  const result = document.getElementById('categories');
   // get the select element's selected option value
   const category = result.options[result.selectedIndex].text;
   console.log({ start_date, end_date, category });
 
   // update the chart
   getChartDataWrapper({ start_date, end_date, category });
+  // reset the radio button to cumulative
 });
 
-const chartRadios = document.getElementsByName("chart-radio");
+const chartRadios = document.getElementsByName('chart-radio');
 for (radio of chartRadios) {
   // remember that arrow function does not bind it's own 'this'
-  radio.addEventListener("click", function(e) {
-    data = { labels: rawChartData.dates, datasets: [] }
+  radio.addEventListener('click', function (e) {
+    data = { labels: rawChartData.dates, datasets: [] };
+    isCumulative = this.id === 'cumulative' ? true : false;
 
-  // build structure for each dataset entry (each line in graph)
-  Object.entries(rawChartData.teamStats).forEach(([key, val], i) => {
+    // build structure for each dataset entry (each line in graph)
+    Object.entries(rawChartData.teamStats).forEach(([key, val], i) => {
       data.datasets.push({
-          label: key,
-          data: this.id === "cumulative" ? val.cumulative : val.rollingAverage,
-          borderColor: rawChartData.colours[i],
-          hidden: teamsHidden[key]
+        label: key,
+        data: isCumulative ? val.cumulative : val.rollingAverage,
+        borderColor: rawChartData.colours[i],
+        hidden: teamsHidden[key],
       });
-  });
-  myChart.destroy();
-  var ctx = document.getElementById('league-graph').getContext('2d');
+    });
+    myChart.destroy();
+    var ctx = document.getElementById('league-graph').getContext('2d');
 
-  myChart = generateChart(ctx, data, rawChartData.category)
+    myChart = generateChart(ctx, data, rawChartData.category);
   });
 }
 
-const selectButtons = document.getElementsByClassName("select-button");
+const selectButtons = document.getElementsByClassName('select-button');
 for (button of selectButtons) {
-  button.addEventListener("click", function(e) {
-    const hide = this.id === "unselect-button" ? true : false;
+  button.addEventListener('click', function (e) {
+    const hide = this.id === 'unselect-button' ? true : false;
     for (dataset of myChart.data.datasets) {
       dataset.hidden = hide;
       teamsHidden[dataset.label] = hide;
     }
     myChart.update();
-  })
+  });
 }
 
 // utility functions
-const getMinMaxDates = async _ => {
+const getMinMaxDates = async (_) => {
   const response = await fetch(buildFetchURL('flatpickr'), {
     method: 'POST',
     headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
   });
   const resData = await response.json();
   return resData;
-}
+};
 
-const buildFetchURL = route => {
-  return window.location.protocol + "//" + window.location.host + "/" + route
-}
+const buildFetchURL = (route) => {
+  return window.location.protocol + '//' + window.location.host + '/' + route;
+};
 
-getMinMaxDates().then(data => {
+getMinMaxDates().then((data) => {
   fp = flatpickr('#datepicker', {
     altInput: true,
-    altFormat: "F j, Y",
-    dateFormat: "Y-m-d",
-    mode: "range",
+    altFormat: 'F j, Y',
+    dateFormat: 'Y-m-d',
+    mode: 'range',
     // maxDate doesn't work properly if not converted to Date first
     minDate: new Date(data.min_date),
-    maxDate: new Date(data.max_date)
+    maxDate: new Date(data.max_date),
   });
-})
-
-
-
+});
--- public/js/chart.js
+++ public/js/chart.js
@@ -8,106 +8,110 @@ var teamsHidden = {};
 const chartContainer = document.getElementById('chart-container');
 
 // reqData is start_date, end_date, and category, or empty (first page load)
-const getChartData = async reqData => {
-    const response = await fetch(buildFetchURL('chart'), {
-        // only post request can have body in fetch API
-        method: 'POST',
-        headers: {
-            // 'Accept': 'application/json',
-            // posting JSON data
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(reqData)
-    });
-    const resData = await response.json();
-    rawChartData = resData;
+const getChartData = async (reqData) => {
+  const response = await fetch(buildFetchURL('chart'), {
+    // only post request can have body in fetch API
+    method: 'POST',
+    headers: {
+      // 'Accept': 'application/json',
+      // posting JSON data
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(reqData),
+  });
+  const resData = await response.json();
+  rawChartData = resData;
 
-    // determine minimum chart size based off size of dataset
-    if (rawChartData.dates.length <= 14) {
-        chartContainer.className += 'small-dataset';
-    } else if (rawChartData.dates.length <= 100) {
-        chartContainer.className += 'medium-dataset';
-    }
+  // determine minimum chart size based off size of dataset
+  if (rawChartData.dates.length <= 14) {
+    chartContainer.className += 'small-dataset';
+  } else if (rawChartData.dates.length <= 100) {
+    chartContainer.className += 'medium-dataset';
+  }
 
-    return resData;
-}
+  return resData;
+};
 
 // use data provided from AJAX request to build chart
 // function expressions are NOT hoisted, so this must appear above any calling code
-const buildChartData = rawData => {
-    data = { labels: rawData.dates, datasets: [] }
+const buildChartData = (rawData) => {
+  data = { labels: rawData.dates, datasets: [] };
 
-    // build structure for each dataset entry (each line in graph)
-    Object.entries(rawData.teamStats).forEach(([key, val], i) => {
-        // also add each key to teamsHidden object, only the first time the page is loaded to avoid overwriting
-        if (Object.keys(teamsHidden).length < Object.keys(rawData.teamStats).length) {
-            teamsHidden[key] = false;
-        }
+  // build structure for each dataset entry (each line in graph)
+  Object.entries(rawData.teamStats).forEach(([key, val], i) => {
+    // also add each key to teamsHidden object, only the first time the page is loaded to avoid overwriting
+    if (
+      Object.keys(teamsHidden).length < Object.keys(rawData.teamStats).length
+    ) {
+      teamsHidden[key] = false;
+    }
 
-        data.datasets.push({
-            label: key,
-            data: val.cumulative,
-            borderColor: rawData.colours[i],
-            hidden: teamsHidden[key]
-        });
+    data.datasets.push({
+      label: key,
+      data: isCumulative ? val.cumulative : val.rollingAverage,
+      borderColor: rawData.colours[i],
+      hidden: teamsHidden[key],
     });
-    return data;
-}
+  });
+  return data;
+};
 
 const generateChart = (ctx, chartData, chartTitle) => {
-    return new Chart(ctx, {
-        type: 'line',
-        data: chartData,
-        options: {
-            legend: {
-                position: 'top',
-                align: 'end',
-                onHover: function(e) {
-                    e.target.style.cursor = 'pointer';
-                 },
-                 onLeave: (e) => {
-                     e.target.style.cursor = 'default';
-                 }, 
-                 onClick: function (e, legendItem) {  
-                     console.log(legendItem);
-                     teamsHidden[legendItem.text] = !teamsHidden[legendItem.text];
-                     Chart.defaults.global.legend.onClick.call(this, e, legendItem);
-                 }
-                //  labels: {
-                //      defaultFontStyle: 'monospace'
-                //  }
+  return new Chart(ctx, {
+    type: 'line',
+    data: chartData,
+    options: {
+      legend: {
+        position: 'top',
+        align: 'end',
+        onHover: function (e) {
+          e.target.style.cursor = 'pointer';
+        },
+        onLeave: (e) => {
+          e.target.style.cursor = 'default';
+        },
+        onClick: function (e, legendItem) {
+          console.log(legendItem);
+          teamsHidden[legendItem.text] = !teamsHidden[legendItem.text];
+          Chart.defaults.global.legend.onClick.call(this, e, legendItem);
+        },
+        //  labels: {
+        //      defaultFontStyle: 'monospace'
+        //  }
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              // y-axis starts at 0
+              // beginAtZero: true
             },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        // y-axis starts at 0
-                        // beginAtZero: true
-                    }
-                }]
-            },
-            // default true
-            responsive: true,
-            maintainAspectRatio: false,
-            title: {
-                display: true,
-                text: chartTitle,
-                fontSize: 24
-            }
-        }
-    });
-}
+          },
+        ],
+      },
+      // default true
+      responsive: true,
+      maintainAspectRatio: false,
+      title: {
+        display: true,
+        text: chartTitle,
+        fontSize: 24,
+      },
+    },
+  });
+};
 
 function getChartDataWrapper(reqData) {
-    getChartData(reqData).then(data => {
-        const chartData = buildChartData(data);
+  getChartData(reqData).then((data) => {
+    const chartData = buildChartData(data);
 
-        if (myChart !== undefined) {
-            myChart.destroy();
-        }
-        var ctx = document.getElementById('league-graph').getContext('2d');
+    if (myChart !== undefined) {
+      myChart.destroy();
+    }
+    var ctx = document.getElementById('league-graph').getContext('2d');
 
-        myChart = generateChart(ctx, chartData, data.category)
-    });
+    myChart = generateChart(ctx, chartData, data.category);
+  });
 }
 
 getChartDataWrapper();
